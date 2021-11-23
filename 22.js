cbda975156757951f63d3b82bff6ca82c0af4aad
AndreyBernacky <dronskmne@gmail.com>
2021-11-23 20:40:01 +0300

    CodeWars tasks 25-28

---
+++ 22.js
@@ -0,0 +1,22 @@
+const a = "dev";
+const b = "Lab";
+
+const name = a + b;
+
+//
+
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
+
+  //
+
+  function move (position, roll) {
+    return position + roll*2;
+  }
+
+  //
+  function opposite(number) {
+  return number*(-1);
+}
