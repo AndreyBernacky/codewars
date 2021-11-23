232395c3ad500c0c1b1044f98ecfa679efa168e6
AndreyBernacky <dronskmne@gmail.com>
2021-11-23 20:39:34 +0300

    CodeWars tasks 21-24

---
+++ 21.js
@@ -0,0 +1,20 @@
+function isDivisible(n, x, y) {
+    if(n % x == 0 && n % y == 0){
+      return true;
+    }else
+      return false;
+  }
+
+    // i will be an integer. Double it and return it.
+
+  function doubleInteger(i) {
+    return i*2;
+  }
+
+  function makeNegative(num) {
+    return num >= 0 ? num*(-1): num;
+  }
+
+  function  multiply(x,y){
+    return x*y;
+  }
