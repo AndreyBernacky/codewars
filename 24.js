87273f0a311027aff163ba5fb0da7e79d5b6cb9d
AndreyBernacky <dronskmne@gmail.com>
2021-11-23 20:40:59 +0300

    CodeWars tasks 33-34

---
+++ 24.js
@@ -0,0 +1,16 @@
+function between(a, b) {
+    const arr = [];
+    for(let i = a; i <= b; i++){
+      arr.push(i);
+    }
+    return arr;
+  }
+
+//
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+// 
+
