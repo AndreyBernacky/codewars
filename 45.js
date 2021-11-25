ea25aae63a9d743a8ed1591b6fd0c3b2d147a263
AndreyBernacky <dronskmne@gmail.com>
2021-11-25 20:04:58 +0300

    CodeWars tasks 85 - 86

---
+++ 45.js
@@ -0,0 +1,11 @@
+function greet(name){
+    if(name == "Johnny")
+      return "Hello, my love!";
+    else 
+      return "Hello, " + name + "!";
+  }
+  //
+  function main(verb, noun){
+    return verb + noun
+  }
+  //
